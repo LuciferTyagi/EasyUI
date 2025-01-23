fcbd912cb32ceda4c8e5bddff4dd7ab24e84da1d
Yagyansh Tyagi <78792172+LuciferTyagi@users.noreply.github.com>
2025-01-23 15:20:49 +0530

    f

--- server/index.js
+++ server/index.js
@@ -21,3 +21,4 @@ app.use('/api/categories', categoryRoutes);
 app.use('/api/items', itemRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
