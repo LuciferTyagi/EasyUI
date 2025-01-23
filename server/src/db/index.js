95868c7fed93593c79be4f41de5cd4876281191a
Yagyansh Tyagi <78792172+LuciferTyagi@users.noreply.github.com>
2025-01-23 17:56:20 +0530

    experiment

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
-
+const MONGODB_URI=`mongodb+srv://yagyanshtyagi61:euk1ocXHTSQ8ljMy@cluster1easyui.yqtxw.mongodb.net`
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`,
+      `${MONGODB_URI}/${DB_NAME}`,
     );
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`,
