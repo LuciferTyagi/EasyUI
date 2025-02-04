a115aa7f2bf4f7d1d4ec1a8875fdae72f758698e
Yagyansh Tyagi <78792172+LuciferTyagi@users.noreply.github.com>
2025-02-04 15:37:59 +0530

    Main Component

--- client/src/Components/BaseComponent.jsx
+++ client/src/Components/BaseComponent.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCopy, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import React, {useState,Suspense  } from 'react'
 import  SyntaxHighlighter  from 'react-syntax-highlighter';
-import { darcula, dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { darcula, docco, dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 
 const ComponentPreview = ({ selectedVariation,selectedCategoryName }) => {
@@ -28,7 +28,7 @@ const DynamicCodeBlock = ({ selectedVariation, selectedCategoryName }) => {
       return {
         default: () => (
           <div className=''>
-          <SyntaxHighlighter language="javascript" style={darcula}>
+          <SyntaxHighlighter language="javascript" style={dracula}>
             {codeString}
           </SyntaxHighlighter>
           </div>
@@ -46,9 +46,7 @@ const DynamicCodeBlock = ({ selectedVariation, selectedCategoryName }) => {
 
 
 const BaseComponent = ({ name , categories }) => {
-        const [selectedTabs, setSelectedTabs] = useState({});
-        
-            
+        const [selectedTabs, setSelectedTabs] = useState({});     
         const copyToClipboard = () => {
         navigator.clipboard.writeText(accordionCode);
       };
@@ -57,59 +55,50 @@ const BaseComponent = ({ name , categories }) => {
         category.items.some(item => item.name === name)
       )
       const selectedItem = selectedCategory?.items.find(item => item.name === name);
-
-      console.log(selectedCategory)
-      console.log("SelectedItem",selectedItem)
-
   return (
-    <div className="flex flex-col gap-4     bg--300 p-4 ">
-            <div className='Component-Info flex flex-col gap-2'>
-      
-                    <p className='text-base font-medium text-[#3E362E]'>{selectedCategory.name}</p>
-                    <p className='text-2xl font-semibold text-[#865D36]'>Tailwind CSS {selectedItem.name}</p>
-                    <p className='text-base font-normal text-[#865D36]'>{selectedItem.generalDetail}</p>
-                    <div className='border-[1px] border-[#3E362E] p-2 rounded-md'>
-                    <div className='flex items-center gap-2 '>
-                        <FontAwesomeIcon icon={faReact}  className='text-[#3E362E] size-7'/>
-                        <p className='text-base font-medium text-[#3E362E]'>Require React JS</p>
-                    </div>
-                    <p className='text-base font-normal text-[#865D36]'>This component requires the use of ReactJS to function properly. Please ensure you have ReactJS integrated into your project. If you're already using ReactJS, you can skip this message.</p>
-                
-                    </div>
-            </div>
-            <div className='Component-type flex flex-col bg--400'>
-                    <div className='Component-Card flex flex-col gap-10'>
-                            {selectedItem.variations.map((variants) =>{
-                                const isPreview = selectedTabs[variants.name] === 'Preview';
-                                return(
-                                <div key={variants.name} className='flex flex-col gap-2'>
-                                <p className='Component-Name text-base font-medium text-[#3E362E]'>{variants.name}</p>
-                                <p className='Component-Work text-base font-normal text-[#865D36]'>{variants.details}</p>
-                                <div className='Option-Container flex justify-between'>
-                                    <div className='Tab-Selector flex gap-1 p-1 rounded bg-[#865D36]'>
-                                            <p className={`text-base cursor-pointer px-2 py-1 rounded ${isPreview  ? "bg-white text-[#865D36]" :"bg-transparent"}`} onClick={()=>setSelectedTabs((prev) =>({
-                                                ...prev,
-                                                [variants.name]: 'Preview',
-                                            }))}>Preview</p>
-                                            <p className={`text-base cursor-pointer px-2 py-1 rounded ${!isPreview  ? "bg-white text-[#865D36]" : "bg-transparent text-white"}`}  onClick={() => setSelectedTabs((prev) =>({
-                                                ...prev,
-                                                
-                                                [variants.name]: 'Code',
+  
+    <div className="Parent flex flex-col gap-4     bg--300  ">
+        <div className='First-Child Component-Info flex flex-col gap-2 pb-10 mb-10 border-b border-zinc-600/20'>
+             <h4 className='text-sm font-semibold text-zinc-900'>{selectedCategory.name}</h4>
+             <h2 className='text-2xl font-semibold text-zinc-700'>Tailwind CSS {selectedItem.name}</h2>
+             <p className='text-lg font-normal text-zinc-500'>{selectedItem.generalDetail}</p>
+             <div className='border-[1px] border-zinc-700/50 p-2 sm:p-6 rounded-lg mt-2'>
+                <div className='flex'>
+                <FontAwesomeIcon icon={faReact}  className='text-zinc-500 size-6 bg--100'/>
+                <div className='ms-3'>
+                   <h4 className='text-base font-semibold text-zinc-700 bg--200 leading-none'>Require React JS</h4>
+                    <p className='text-base font-normal text-zinc-500 mt-2'>This component requires the use of ReactJS to function properly. Please ensure you have ReactJS integrated into your project. If you're already using ReactJS, you can skip this message.</p>
+                </div>
+                </div>
+             </div>
+        </div>
+       
+        <div className='Second-Child Component-type   bg--400'>
+            <div className='Component-Card flex flex-col gap-10 '>
+                  {selectedItem.variations.map((variants) =>{
+                      const isPreview = selectedTabs[variants.name] === 'Preview';
+                      return(
+                      <div key={variants.name} className='flex flex-col gap-2'>
+                      <h2 className='Component-Name text-lg font-bold text-zinc-700'>{variants.name}</h2>
+                      <p className='Component-Work text-base font-normal text-zinc-500'>{variants.details}</p>
+                      <div className='Option-Container flex justify-between '>
+                            <div className='Tab-Selector flex gap-1 p-1 rounded bg-slate-900'>
+                                  <button className={`text-base cursor-pointer px-2 py-1 rounded ${isPreview  ? "bg-white text-zinc-700" :"bg-transparent text-white"}`} onClick={()=>setSelectedTabs((prev) =>({...prev,[variants.name]: 'Preview',}))}>Preview</button>
+                                  <button className={`text-base cursor-pointer px-2 py-1 rounded ${!isPreview  ? "bg-white text-zinc-700" : "bg-transparent text-white"}`}  onClick={() => setSelectedTabs((prev) =>({...prev,  [variants.name]: 'Code', }))}>Code</button>
+                            </div>
 
-                                            }))}>Code</p>
-                                    </div>
-                                    <div className='Copy-Edit flex items-center gap-4 border-[1px] border-[#3E362E] rounded p-2 cursor-pointer'>
-                                                <div className='Copy-Button flex items-center gap-1 ' onClick={copyToClipboard}>
-                                                        <FontAwesomeIcon icon={faCopy} className='text-[#865D36]'/>
-                                                        <p className='text-base font-light text-[#3E362E]'>Copy</p>
-                                                </div>
-                                                <div className='h-full w-[1px] bg-[#3E362E]'>
 
-                                                </div>
-                                                <FontAwesomeIcon icon={faPenToSquare} className='text-[#865D36]'/>
-                                    </div>
-                                </div>
-                                <div className={`Component-Body w-auto h-[400px] overflow-y-scroll bg--600 border-[1px] border-[#3E362E] rounded ${!isPreview ?"" :"p-4"} `}>
+                            <div className='Copy-Edit flex items-center gap-4 border-[1px] border-zinc-600/50 rounded-lg p-2 cursor-pointer'>
+                                  <div className='Copy-Button flex items-center gap-1 ' onClick={copyToClipboard}>
+                                        <FontAwesomeIcon icon={faCopy} className='text-zinc-500'/>
+                                        <span className='text-base  text-zinc-500'>Copy</span>
+                                   </div>
+                                  <div className='h-full w-[1px] bg-zinc-700/50'></div>
+                                  <FontAwesomeIcon icon={faPenToSquare} className='text-zinc-500'/>
+                            </div>
+                       </div>
+
+                       <div className={`Component-Body   overflow-y-scroll bg--600  rounded-xl shadow-sm border  ${!isPreview  ? "h-[400px]" :"h-auto p-6"} `}>
                                 {isPreview   ? ( 
                                 <ComponentPreview selectedVariation={variants.name} selectedCategoryName={selectedItem.name} />    
                                 ) : (
@@ -120,15 +109,16 @@ const BaseComponent = ({ name , categories }) => {
                               />
                                 </pre> 
                                 )}
-                                  </div>
+                        </div>
                                
-                                </div>
-                                )})}
+                        </div>
+                  )})}
                         
-                    </div>
             </div>
+        </div>
            
     </div>
+   
   )
 }
 
--- client/src/Components/Component.jsx
+++ client/src/Components/Component.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAddressCard, faArrowRight, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faAddressCard, faArrowRight, faX, faXmark } from '@fortawesome/free-solid-svg-icons';
 import PageInfo from './PageInfo';
 import BaseComponent from './BaseComponent';
 import { sidebarItems } from '../Utils/Constant';
 import Footer from "../Components/Footer"
 const Component = () => {
-    const[SideBar ,setSideBar] = useState(false);
+    const[sideBar ,setSideBar] = useState(false);
     const [selectedItem, setSelectedItem] = useState("About this Page");
     const [categories , setCategories] = useState([]);
     const sidebarRef = useRef();
@@ -27,15 +27,49 @@ const Component = () => {
     },[]);
   return (
 
-    <section className='w-full bg-gradient-to-tr bg-white min-h-screen pt-4 px-8 lg:pt-10 font-inter '>
-             <button className='lg:hidden ml-auto mr-4 flex items-center justify-center gap-2 py-2 px-4 bg-white rounded-xl font-inter outline-none border border-transparent shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]'>
+    <section className=' w-full bg-gradient-to-tr bg-red-800 min-h-screen pt-4 px-4 md:px-8 lg:pt-10 font-inter '>
+             <button onClick={() =>setSideBar(!sideBar)} className='lg:hidden ml-auto m flex items-center justify-center gap-2 py-2 px-4 bg-white rounded-xl font-inter outline-none border border-transparent shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]'>
              <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <path d="M17 8L21 12L17 16M3 12H13M3 6H13M3 18H13"></path>
               </svg>
               Sidebar
              </button>
-             <div className='bg-green-300 max-w-[1400px] mx-auto lg:grid lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10 '>
-                <aside class=" z-30  sticky top-0  h-screen w-full shrink-0  lg:block lg:self-start bg-red-300 pr-6 pt-6">
+             <div className=' bg-green-300    max-w-[1400px] mx-auto lg:grid lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10 transition ease-in duration-300'>
+                {sideBar && (   
+                    <div className=' fixed  inset-0 z-[200] backdrop-blur-sm lg:hidden'>          
+                    <aside class=" h-screen w-full bg-red-400 sm:w-1/2">
+                    <nav className='relative h-screen overflow-y-scroll bg-yellow-300  pt-4 pl-4  text-sm'> 
+                        <FontAwesomeIcon icon={faX} className='absolute top-4 right-4 text-zinc-500 size-4' onClick={() => setSideBar(false)}/>                 
+                        {sidebarItems.map((item) =>(
+                            <ul key={item.title}>
+                                <li>
+                                    <h4 className='font-semibold text-zinc-700 mb-2'>{item.title}</h4>
+                                    <ul className='space-y-2 mb-4'>
+                                        {item.links.map((link) =>(
+                                            <li className='text-zinc-500 bg--200    ' key={link.name}><a href={link.url} className='text-zinc-500 '>{link.name}</a></li>
+                                        ))}
+                                    </ul>
+                                </li>
+                            </ul>
+                        ))}
+                        {categories.map((category) =>(
+                            <ul>
+                                <li>
+                                    <h4 className='font-semibold text-zinc-700 mb-2'>{category.name}</h4>
+                                    <ul className='space-y-2 mb-4'>
+                                        {category.items.map((item) =>(
+                                            <li onClick={() => setSelectedItem(item.name)} className='text-zinc-500' ><span onClick={()=> setSideBar(false)} >{item.name}</span></li>
+                                        ))}
+                                    </ul>
+                                </li>
+                            </ul>
+                        ))}
+                    </nav>
+                   </aside>
+                   </div>
+                )}
+                
+                <aside class="hidden z-30  sticky top-0  h-screen w-full shrink-0  lg:block lg:self-start bg--300 pr-6 pt-">
                     <nav className='h-full bg-yellow-300 overflow-y-scroll   text-sm'>                  
                         {sidebarItems.map((item) =>(
                             <ul key={item.title}>
